d1f062d4825269e50d331297fcd827615d3b0a9f
Federico Canet <fedeea@gmail.com>
2015-10-07 21:36:31 -0300

    Corregidos tildes.

--- FarmaciaYa/src/com/is3/EnvioCorreo.java
+++ FarmaciaYa/src/com/is3/EnvioCorreo.java
@@ -28,7 +28,7 @@ public class EnvioCorreo {
 		
 		String urlLink = Parameters.getParameter("urlRecoverPassword")+"?userid="+correo;
 		
-		String emailBody = Parameters.getParameter("recuperarPasswordBody").replace("#link#","<a href='"+urlLink+"'>aquí</a>");
+		String emailBody = Parameters.getParameter("recuperarPasswordBody").replace("#link#","<a href='"+urlLink+"'>aqui</a>");
 		mailMessage.setContent(emailBody, "text/html");
 		EnvioCorreo.enviarCorreo(mailMessage);
 		System.out.println("\n\n ===> Your Java Program has just sent an Email successfully. Check your email..");
