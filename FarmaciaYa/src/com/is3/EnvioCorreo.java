3e148beb410f8bb9c0ab1fa175a047324f5ad1bb
Federico Canet <fedeea@gmail.com>
2015-10-14 22:03:50 -0300

    arreglada codificación tilde aquí.

--- FarmaciaYa/src/com/is3/EnvioCorreo.java
+++ FarmaciaYa/src/com/is3/EnvioCorreo.java
@@ -43,7 +43,7 @@ public class EnvioCorreo {
 		
 		String urlLink = Parameters.getParameter("urlConfirmarUsuario")+"?userid="+correo;
 		
-		String emailBody = Parameters.getParameter("confirmarUsuarioBody").replace("#link#","<a href='"+urlLink+"'>aquí</a>");
+		String emailBody = Parameters.getParameter("confirmarUsuarioBody").replace("#link#","<a href='"+urlLink+"'>aqui</a>");
 		mailMessage.setContent(emailBody, "text/html");
 		EnvioCorreo.enviarCorreo(mailMessage);
 		System.out.println("\n\n ===> Your Java Program has just sent an Email successfully. Check your email..");
