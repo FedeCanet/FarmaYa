09f9ba0a310b0475b752cdf53f1626c24e72dd10
Federico Canet <fedeea@gmail.com>
2015-10-20 21:28:36 -0300

    Corregido persitence

--- FarmaciaYa/src/com/is3/LoginServlet.java
+++ FarmaciaYa/src/com/is3/LoginServlet.java
@@ -1,9 +1,6 @@
 package com.is3;
 
-import javax.servlet.Servlet;
-
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import com.is3.PersistenceHelper;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
