30a23cdf4c68b827e6daaa4a16fdcc3888f545b1
Nicolas Fernandez <jnfgit@gmail.com>
2015-10-23 21:38:40 -0300

    orden

--- FarmaciaYa/src/com/is3/LoginServlet.java
+++ FarmaciaYa/src/com/is3/LoginServlet.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.is3.bo.Orden;
+
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -39,6 +41,16 @@ public class LoginServlet extends HttpServlet {
 			response.addCookie(userName);
 			response.sendRedirect("busqueda.jsp");
 			//response.sendRedirect("LoginSuccess.jsp");
+			//Agregar una ORDEN VACIA
+			if(session.getAttribute("orden") != null){
+				//Ya tiene una orden
+				
+			}else{
+				//creamos una nueva Orden
+				Orden o = new Orden();
+				
+			}
+			
 		}else{
 			//RequestDispatcher rd = getServletContext().getRequestDispatcher("/inicio.jsp");//("/login.html");
 			request.setAttribute("errorMessage", "Usuario y/o Password incorrectos.");
